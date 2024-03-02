9308928a98c2972a1ee169a629357b05b058e178
Kelvin Lin <kelvin.lin@stonybrook.edu>
2024-03-02 18:04:37 -0500

    Limited MongoDB implementation

--- server/models/questions.js
+++ server/models/questions.js
@@ -3,12 +3,6 @@ var Schema = mongoose.Schema;
 
 var QuestionsSchema = new Schema({
     text: {type:String, required:true},
-    choices: [{type: String, required:true}],
-    correctAnswer: {type: String, required: true},
-})
-
-QuestionsSchema.virtual('url').get(function(){
-    return 'post/questions/_id' + this.id;
 })
 
 module.exports = mongoose.model('Questions', QuestionsSchema);
